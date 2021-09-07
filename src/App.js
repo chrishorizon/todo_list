567267b093127848bff50f694e493dcf638f4cc6
Chris <christhehorizon@gmail.com>
2021-09-06 20:00:45 -0700

    todo list updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
 
 function App() {
+  const [newTodo, setNewTodo] = useState(''); // inputs
+  const [todos, setTodos] = useState([]); // saves inputs
+
+  const handleNewTodoSubmit = (event) => {
+    event.preventDefault();
+    setTodos([...todos, newTodo]) // spread operator takes an array on the right side of ... and separates it by individual items and put into a new array and , one last item added on the end
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center"}}>
+      <form onSubmit={(event) => {
+        handleNewTodoSubmit(event);
+      }}>
+        <input onChange={(event) => {
+          setNewTodo(event.target.value);
+        }} type="text" />
+        <div>
+          <button>Add</button>
+        </div>
+      </form>
     </div>
   );
 }
